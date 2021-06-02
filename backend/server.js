ae6dca682df81fdd964425fe291f919c68991507
Samed Skulj <samed.skulj08@gmail.com>
2021-06-02 20:03:40 +0200

    Custom error handling za rute - 404, 500...

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,16 @@ const app = express();
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errormidleware.js";
 connectDB();
 dotenv.config();
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
